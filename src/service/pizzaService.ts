ef9f26fc31fbd722d79fc61f16bbb2bab1dddccd
milkslurpee <johnsuorsa@gmail.com>
2026-02-19 03:57:28 -0700

    initialized

--- src/service/pizzaService.ts
+++ src/service/pizzaService.ts
@@ -1,115 +1,140 @@
 enum Role {
-  Diner = 'diner',
-  Franchisee = 'franchisee',
-  Admin = 'admin',
+	Diner = "diner",
+	Franchisee = "franchisee",
+	Admin = "admin",
 }
 
 namespace Role {
-  export function isRole(user: User | null, role: Role): boolean {
-    return user != null && Array.isArray(user.roles) && !!user.roles.find((r) => r.role === role);
-  }
+	export function isRole(user: User | null, role: Role): boolean {
+		return (
+			user != null &&
+			Array.isArray(user.roles) &&
+			!!user.roles.find((r) => r.role === role)
+		);
+	}
 }
 
 type Menu = Pizza[];
 
 type Pizza = {
-  id: string;
-  title: string;
-  description: string;
-  image: string;
-  price: number;
+	id: string;
+	title: string;
+	description: string;
+	image: string;
+	price: number;
 };
 
 type OrderItem = {
-  menuId: string;
-  description: string;
-  price: number;
+	menuId: string;
+	description: string;
+	price: number;
 };
 
 type Order = {
-  id: string;
-  franchiseId: string;
-  storeId: string;
-  date: string;
-  items: OrderItem[];
+	id: string;
+	franchiseId: string;
+	storeId: string;
+	date: string;
+	items: OrderItem[];
 };
 
 type OrderResponse = {
-  order: Order;
-  jwt: string;
+	order: Order;
+	jwt: string;
 };
 
 type OrderHistory = {
-  id: string;
-  dinerId: string;
-  orders: Order[];
+	id: string;
+	dinerId: string;
+	orders: Order[];
 };
 
 type UserRole = {
-  role: Role;
-  objectId?: string;
+	role: Role;
+	objectId?: string;
 };
 
 type User = {
-  id?: string;
-  name?: string;
-  email?: string;
-  password?: string;
-  roles?: UserRole[];
+	id?: string;
+	name?: string;
+	email?: string;
+	password?: string;
+	roles?: UserRole[];
 };
 
 type Store = {
-  id: string;
-  name: string;
-  totalRevenue?: number;
+	id: string;
+	name: string;
+	totalRevenue?: number;
 };
 
 type Franchise = {
-  id: string;
-  admins?: { email: string; id?: string; name?: string }[];
-  name: string;
-  stores: Store[];
+	id: string;
+	admins?: { email: string; id?: string; name?: string }[];
+	name: string;
+	stores: Store[];
 };
 
 type FranchiseList = {
-  franchises: Franchise[];
-  more: boolean;
+	franchises: Franchise[];
+	more: boolean;
 };
 
 type Endpoint = {
-  requiresAuth: boolean;
-  method: string;
-  path: string;
-  description: string;
-  example: string;
-  response: any;
+	requiresAuth: boolean;
+	method: string;
+	path: string;
+	description: string;
+	example: string;
+	response: any;
 };
 
 type Endpoints = {
-  endpoints: Endpoint[];
+	endpoints: Endpoint[];
 };
 
 type JWTPayload = {
-  message: string;
-  payload: string;
+	message: string;
+	payload: string;
 };
 
 interface PizzaService {
-  login(email: string, password: string): Promise<User>;
-  register(email: string, password: string, role: string): Promise<User>;
-  logout(): void;
-  getUser(): Promise<User | null>;
-  getMenu(): Promise<Menu>;
-  getOrders(user: User): Promise<OrderHistory>;
-  order(order: Order): Promise<OrderResponse>;
-  verifyOrder(jwt: string): Promise<JWTPayload>;
-  getFranchise(user: User): Promise<Franchise[]>;
-  createFranchise(franchise: Franchise): Promise<Franchise>;
-  getFranchises(page: number, limit: number, nameFilter: string): Promise<FranchiseList>;
-  closeFranchise(franchise: Franchise): Promise<void>;
-  createStore(franchise: Franchise, store: Store): Promise<Store>;
-  closeStore(franchise: Franchise, store: Store): Promise<null>;
-  docs(docType: string): Promise<Endpoints>;
+	login(email: string, password: string): Promise<User>;
+	register(email: string, password: string, role: string): Promise<User>;
+	logout(): void;
+	getUser(): Promise<User | null>;
+	getMenu(): Promise<Menu>;
+	getOrders(user: User): Promise<OrderHistory>;
+	order(order: Order): Promise<OrderResponse>;
+	verifyOrder(jwt: string): Promise<JWTPayload>;
+	getFranchise(user: User): Promise<Franchise[]>;
+	createFranchise(franchise: Franchise): Promise<Franchise>;
+	getFranchises(
+		page: number,
+		limit: number,
+		nameFilter: string,
+	): Promise<FranchiseList>;
+	closeFranchise(franchise: Franchise): Promise<void>;
+	createStore(franchise: Franchise, store: Store): Promise<Store>;
+	closeStore(franchise: Franchise, store: Store): Promise<null>;
+	docs(docType: string): Promise<Endpoints>;
+	updateUser(user: User): Promise<User>;
 }
 
-export { Role, PizzaService, User, Menu, Pizza, OrderHistory, Order, Franchise, FranchiseList, Store, OrderItem, Endpoint, Endpoints, OrderResponse, JWTPayload };
+export {
+	Role,
+	PizzaService,
+	User,
+	Menu,
+	Pizza,
+	OrderHistory,
+	Order,
+	Franchise,
+	FranchiseList,
+	Store,
+	OrderItem,
+	Endpoint,
+	Endpoints,
+	OrderResponse,
+	JWTPayload,
+};
--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -551,6 +551,48 @@ test("franchise, about, and history", async ({ page }) => {
 	await page.getByRole("link", { name: "History" }).click();
 });
 
+test("updateUser", async ({ page }) => {
+	const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
+	const newEmail = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
+	await page.goto("/");
+	await page.getByRole("link", { name: "Register" }).click();
+	await page.getByRole("textbox", { name: "Full name" }).fill("pizza diner");
+	await page.getByRole("textbox", { name: "Email address" }).fill(email);
+	await page.getByRole("textbox", { name: "Password" }).fill("diner");
+	await page.getByRole("button", { name: "Register" }).click();
+	await page.getByRole("link", { name: "pd" }).click();
+	await expect(page.getByRole("main")).toContainText("pizza diner");
+
+	await page.getByRole("button", { name: "Edit" }).click();
+	await expect(page.locator("h3")).toContainText("Edit user");
+	await page.getByRole("textbox").first().fill("pizza dinerx");
+	await page.getByRole("button", { name: "Update" }).click();
+	await page.waitForSelector('[role="dialog"].hidden', { state: "attached" });
+	await expect(page.getByRole("main")).toContainText("pizza dinerx");
+
+	await page.getByRole("button", { name: "Edit" }).click();
+	await expect(page.locator("h3")).toContainText("Edit user");
+	await page.getByRole("textbox").last().fill("derpSauce");
+	await page.getByRole("button", { name: "Update" }).click();
+	await page.waitForSelector('[role="dialog"].hidden', { state: "attached" });
+	await expect(page.getByRole("main")).toContainText("pizza dinerx");
+
+	await page.getByRole("button", { name: "Edit" }).click();
+	await expect(page.locator("h3")).toContainText("Edit user");
+	await page.getByRole("textbox").nth(1).fill(newEmail);
+	await page.getByRole("button", { name: "Update" }).click();
+	await page.waitForSelector('[role="dialog"].hidden', { state: "attached" });
+	await expect(page.getByRole("main")).toContainText("pizza dinerx");
+
+	await page.getByRole("link", { name: "Logout" }).click();
+	await page.getByRole("link", { name: "Login" }).click();
+	await page.getByRole("textbox", { name: "Email address" }).fill(newEmail);
+	await page.getByRole("textbox", { name: "Password" }).fill("derpSauce");
+	await page.getByRole("button", { name: "Login" }).click();
+	await page.getByRole("link", { name: "pd" }).click();
+	await expect(page.getByRole("main")).toContainText("pizza dinerx");
+});
+
 async function login(page: Page, email: string, password: string) {
 	await page.getByRole("link", { name: "Login" }).click();
 	await page.getByRole("textbox", { name: "Email address" }).fill(email);
